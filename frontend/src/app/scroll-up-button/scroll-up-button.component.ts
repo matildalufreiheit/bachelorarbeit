7bbe390b90efe2b3bd07fca4559d844646671155
Lu Freiheit <lufreiheit@eduroam-141-23-186-221.wlan.tu-berlin.de>
2024-11-01 11:15:52 +0100

    scroll-up componente done

--- frontend/src/app/scroll-up-button/scroll-up-button.component.ts
+++ frontend/src/app/scroll-up-button/scroll-up-button.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-up-button',
   templateUrl: './scroll-up-button.component.html',
   styleUrls: ['./scroll-up-button.component.css']
 })
-export class ScrollUpButtonComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ScrollUpButtonComponent {
+  scrollUp(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-
 }
+
